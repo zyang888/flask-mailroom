0b14345a7e642960ee2bff65fb45c6cc91769ca5
Zhengtang Yang <zyang888@uw.edu>
2018-08-03 07:12:19 -0700

    lesson01 repo

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Flask Mailroom Application
 
-Running at [http://afternoon-reef-51666.herokuapp.com/donations/](http://afternoon-reef-51666.herokuapp.com/donations/).
+Running at [http://ancient-brushlands-28077.herokuapp.com/donations/](http://afternoon-reef-51666.herokuapp.com/donations/).
 
 ## Your Task
 
--- main.py
+++ main.py
@@ -3,9 +3,11 @@ import base64
 
 from flask import Flask, render_template, request, redirect, url_for, session
 
-from model import Donation 
+from model import Donation, Donor
 
 app = Flask(__name__)
+app.secret_key = os.environ.get('SECRET_KEY').encode()
+# app.run(debug=True)
 
 @app.route('/')
 def home():
@@ -15,6 +17,21 @@ def home():
 def all():
     donations = Donation.select()
     return render_template('donations.jinja2', donations=donations)
+
+@app.route('/create', methods=['GET', 'POST'])
+def create():
+
+	if request.method == 'POST':
+		donor = Donor.select().where(Donor.name == request.form['name'])
+		if donor.exists():
+			donation = Donation(donor=donor.get(),value=int(request.form['amount']))
+			donation.save()
+			return redirect(url_for('all'))
+		else:
+			return render_template('create.jinja2', error='Could not find donor.')
+	else:
+		return render_template('create.jinja2')
+
     
 
 if __name__ == "__main__":
